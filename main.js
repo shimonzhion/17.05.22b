e8bb04977bcfdb452925ee79b63a59c06e6d0215
shimonzhion <brys94204@gmail.com>
2022-05-18 01:28:17 +0300

    -

---
+++ main.js
@@ -0,0 +1,165 @@
+// function array1(array) {
+//   var arr = [];
+//   for (var i = 0; i < array.length; i++) {
+//     if (array[i] > 20) {
+//       arr.push(array[i]);
+//     }
+//   }
+//  return arr ;
+// }
+// console.log (array1([2, 52, 10]));
+
+
+// function array2(array){
+//     var array3=[]
+//     for (var i =0;i<array.length;i++){
+//     array3.push(i +"."+ array[i] )
+//     }
+//     return array3 
+// }
+// console.log(array2([2,3,4,5,6,7,8]))
+
+
+// function array3 (array){
+//     var array4=[]
+//     for (var i=0;i<array.length;i++){
+//         if(array[i]%2==0){
+//          array[i]="even"
+//         }
+//     }
+// return array
+// }
+// console.log(array3([2,3,4,5,6,7]))
+
+
+// function numbers (x){
+//     for(var i =0; i<2; i++){
+//      var num= +prompt("enter number")
+//      console.log(num)}
+// }
+// numbers()
+
+// function array4 (array){
+//   for (var i=0; i<array.length;i++){
+// if (array[i]>= 13){
+//   array[i]= "bar-mitzva"
+// }
+// else if (array[i]==12){
+//   array[i] = "bat-mitzva"
+// }
+// else{
+//   array[i]="kid"
+// }
+//   }
+// return array
+// }
+// console.log(array4([12,13,4]))
+
+// function userinfo (age,name,lastname,num){
+
+
+//     if (age>18){
+//       for(var i =0;i<num;i++){
+//       console.log(name, lastname)
+//     }
+    
+//   }
+//   else{
+//     console.log(age,name,lastname,num)
+//   }
+
+
+// }
+// userinfo(19,"shimon","berry",5)
+
+// function thousand (){
+//   for (var i=0; i<1000;i++){
+//   console.log("java script is not java")
+// }
+// }
+// thousand()
+
+// function numName (){
+
+//  var name = prompt ("enter name" )
+//  var num = +prompt ("enter num")
+//  for (var i=0; i<num ;i++){
+//    console.log(name)
+//  }
+// }
+// numName()
+
+// var email = prompt("enter email")
+// var name = prompt("enter name ")
+// var age =+prompt("enter age")
+// var num = +prompt("enter num")
+// if (age >18){
+//   for (var i =0; i<num ;i++){
+//     console.log(email,name,age,)
+//   }
+// }
+// var num = +prompt("enter num")
+// var name2 = prompt("enter name")
+// var myname = "shimon"
+// if (name2 == myname){
+// for (var i =0; i<num;i++){
+//   console.log("hi boss"+ " " +myname)
+// }
+// }
+// else{
+//   console.log("no entry")
+// }
+
+//  var num1 = +prompt("enter num")
+//  var num2 = +prompt("enter num2")
+// for (var i =0;i<num1;i++){
+//   for(var j=0;j<num2;j++){
+//     console.log(j*i)
+//   }
+// }
+// var arraynum =[1,2,3,4,5]
+// for (var i = 0; i<arraynum.length;i++){
+//   console.log(arraynum[i])
+// }
+
+
+// var arrayname =["shimon","daniel","batel","shlomo","aviel"]
+// for (var i=0;i<arrayname.length;i++){
+//   console.log(arrayname[i])
+// }
+
+// var array=[88,99,23,44,55,22,47]
+// for (var i=0; i<array.length;i++){
+//   if(array[i]%2==0){
+//     console.log(array[i])
+//   }
+// }
+// function arrayuser (){
+//   var array = [] 
+//   for (var i=0;i<5;i++){
+  
+//     var prompt1= prompt("enter name")
+   
+//     array.push(prompt1)
+  
+//   }
+//   console.log(array)
+// }
+// arrayuser()
+
+
+for (var i=1;i<100;i++){
+   if (i%3==0 && i%5==0){
+    console.log("fizz buzz")
+  }
+ else if (i%3==0){
+    console.log("fizz")
+  }
+  else if(i%5==0){
+    console.log("buzz")
+  }
+  else{
+    console.log(i)
+  }
+}
+
